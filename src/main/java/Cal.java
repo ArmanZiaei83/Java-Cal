418b635ce0a9a1b482218fd5df862378960a42b7
Arman Ziaei <armanziaei83@gmail.com>
2020-06-11 14:59:22 +0430

    Feat Minus Method : minus method created for minus process .

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -2,10 +2,14 @@ public class Cal {
     public static void main(String[] args){
         Cal a = new Cal();
         a.sum(5,6);
+        a.minus(4,2);
     }
     public void sum (int x , int y ){
         int c = x + y;
         System.out.println("The Sum Result : " + c);
     }
-
+    public void minus(int x , int y){
+        int c = x - y ;
+        System.out.println("The Minus Result : " + c);
+    }
 }
