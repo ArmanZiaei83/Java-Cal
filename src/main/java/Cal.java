0616b479274c3fa54b540a0b2d31e714dcef3ebe
Arman Ziaei <armanziaei83@gmail.com>
2020-06-11 14:56:58 +0430

    Feat Sum Method : the sum method added for sumation process .

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,6 +1,11 @@
 public class Cal {
     public static void main(String[] args){
         Cal a = new Cal();
+        a.sum(5,6);
     }
-    
+    public void sum (int x , int y ){
+        int c = x + y;
+        System.out.println("The Sum Result : " + c);
+    }
+
 }
